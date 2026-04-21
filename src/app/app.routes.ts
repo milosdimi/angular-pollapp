ceca5ed4c0b87702ff67152ac0122e6f3c64f5ff
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-04-21 14:40:45 +0200

    home survey create survey created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { SurveyDetail } from './pages/survey-detail/survey-detail';
+import { CreateSurvey } from './pages/create-survey/create-survey';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'survey/:id', component: SurveyDetail },
+  { path: 'create', component: CreateSurvey },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/survey-detail/survey-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-detail',
+  imports: [],
+  templateUrl: './survey-detail.html',
+  styleUrl: './survey-detail.scss',
+})
+export class SurveyDetail {}
